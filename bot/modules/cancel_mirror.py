3e43e1135f53a81702ab280f28e9c22706869424
ALEXXD1 <54359715+ALEXXD1@users.noreply.github.com>
2021-09-22 13:11:03 +0530

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -28,11 +28,11 @@ def cancel_mirror(update, context):
             except:
                 pass
     if len(args) == 1:
-        msg = f"Please reply to the <code>/{BotCommands.MirrorCommand}</code> message which was used to start the download or send <code>/{BotCommands.CancelMirror} GID</code> to cancel it!"
+        msg = f"Please reply to the <code>/{BotCommands.LeechCommand}</code> message which was used to start the download or send <code>/{BotCommands.CancelMirror} GID</code> to cancel it!"
         if mirror_message and mirror_message.message_id not in keys:
-            if BotCommands.MirrorCommand in mirror_message.text or \
-               BotCommands.TarMirrorCommand in mirror_message.text or \
-               BotCommands.UnzipMirrorCommand in mirror_message.text:
+            if BotCommands.LeechCommand in mirror_message.text or \
+               BotCommands.TarLeechCommand in mirror_message.text or \
+               BotCommands.UnzipLeechCommand in mirror_message.text:
                 msg1 = "Mirror Already Have Been Cancelled"
                 sendMessage(msg1, context.bot, update)
             else:
