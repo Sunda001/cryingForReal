4587071bb8d3f04c021ee2458b9d79b2d3f19075
ALEXXD1 <54359715+ALEXXD1@users.noreply.github.com>
2021-09-23 09:36:13 +0530

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -200,19 +200,6 @@ input[type="submit"]:hover, input[type="submit"]:focus{
 <body>
   <!--© Designed and coded by @bipuldey19-Telegram-->
     <header>
-      <div class="brand">
-        <img
-          src="https://telegra.ph/file/a97fb49ff2498018a59a7.png"
-          alt="logo"
-        />
-        <a href="https://t.me/SlamMirrorUpdates"> 
-          <h2 class="name">slam-mirrorbot</h2>
-        </a>
-      </div>
-      <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
-      </div>
     </header>
     <section>
       <h2 class="intro">Select the files you want to download</h2>
@@ -525,19 +512,6 @@ section span{
 <body>
    <!--© Designed and coded by @bipuldey19-Telegram-->
     <header>
-      <div class="brand">
-        <img
-          src="https://telegra.ph/file/a97fb49ff2498018a59a7.png"
-          alt="logo"
-        />
-        <a href="https://t.me/SlamMirrorUpdates">
-          <h2 class="name">slam-mirrorbot</h2>
-        </a>
-      </div>
-      <div class="social">
-        <a href="https://github.com/SlamDevs/slam-mirrorbot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/SlamMirrorUpdates"><i class="fab fa-telegram"></i></a>
-      </div>
     </header>
     <section>
       <form action="{form_url}">
@@ -560,7 +534,7 @@ section span{
 """
 
 
-@routes.get('/slam/files/{hash_id}')
+@routes.get('/PublicLeech/files/{hash_id}')
 async def list_torrent_contents(request):
 
     torr = request.match_info["hash_id"]
@@ -568,7 +542,7 @@ async def list_torrent_contents(request):
     gets = request.query
 
     if "pin_code" not in gets.keys():
-        rend_page = code_page.replace("{form_url}", f"/slam/files/{torr}")
+        rend_page = code_page.replace("{form_url}", f"/PublicLeech/files/{torr}")
         return web.Response(text=rend_page, content_type='text/html')
 
     client = qba.Client(host="localhost", port="8090",
@@ -654,7 +628,7 @@ async def re_verfiy(paused, resumed, client, torr):
     return True
 
 
-@routes.post('/slam/files/{hash_id}')
+@routes.post('/PublicLeech/files/{hash_id}')
 async def set_priority(request):
 
     torr = request.match_info["hash_id"]
@@ -705,7 +679,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See slam-mirrorbot <a href='https://github.com/SlamDevs/slam-mirrorbot'>@GitHub</a> By <a href='https://github.com/SlamDevs'>SlamDevs</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>Join Public-Leech <a href='t.me/PublicLeechCloneGroup'>@Telegram</a>
 
 
 async def e404_middleware(app, handler):
@@ -715,11 +689,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>Public-Leech</h3>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>slam-mirrorbot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>Public-Leech</h3>", content_type="text/html")
             raise
     return middleware_handler
 
