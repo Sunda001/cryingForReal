4d1b5a823d7fdae9bf1065844d0ef0468603ef58
ALEXXD1 <54359715+ALEXXD1@users.noreply.github.com>
2021-10-12 09:37:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,7 +19,9 @@ from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_tim
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
 from .modules import authorize, mirror_status, mirror, watch, shell, eval, speedtest, leech_settings, cancel_mirror
+from datetime import date
 
+today = date.today()
 
 def stats(update, context):
     currentTime = get_readable_time(time.time() - botStartTime)
@@ -32,8 +34,10 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
+    d1 = today.strftime("%d/%m/%Y")
     stats = f'<b>Bot Uptime:</b> <code>{currentTime}</code>\n' \
             f'<b>Total Disk Space:</b> <code>{total}</code>\n' \
+            f'<b>Date:</b> <code>{d1}</code>\n' \
             f'<b>Used:</b> <code>{used}</code> ' \
             f'<b>Free:</b> <code>{free}</code>\n\n' \
             f'<b>Upload:</b> <code>{sent}</code>\n' \
