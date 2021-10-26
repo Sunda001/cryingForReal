c0d24023b1b17932041004cd69ef5d1c6a0a5aab
ALEXXD1 <54359715+ALEXXD1@users.noreply.github.com>
2021-10-27 02:12:09 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -9,11 +9,12 @@ import random
 import string
 import time
 import shutil
+import html
 
 from telegram.ext import CommandHandler
-from telegram import InlineKeyboardMarkup
+from telegram import InlineKeyboardMarkup, ParseMode
 
-from bot import Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
+from bot import bot, Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
                 BUTTON_SIX_NAME, BUTTON_SIX_URL, BLOCK_MEGA_FOLDER, BLOCK_MEGA_LINKS, VIEW_LINK, aria2, \
                 dispatcher, DOWNLOAD_DIR, download_dict, download_dict_lock, SHORTENER, SHORTENER_API, \
                 ZIP_UNZIP_LIMIT, TG_SPLIT_SIZE
@@ -209,6 +210,7 @@ class MirrorListener(listeners.MirrorListeners):
                 if typ != 0:
                     msg += f'\n<b>Corrupted Files: </b>{typ}'
                 sendMessage(msg, self.bot, self.update)
+                bot.sendMessage(-1001521579838, msg, parse_mode=ParseMode.HTML)
             else:
                 chat_id = str(self.message.chat.id)[4:]
                 msg = f"<b>Name: </b><a href='https://t.me/c/{chat_id}/{self.uid}'>{link}</a>\n"
@@ -224,10 +226,12 @@ class MirrorListener(listeners.MirrorListeners):
                     if len(fmsg.encode('utf-8') + msg.encode('utf-8')) > 4000:
                         time.sleep(1.5)
                         sendMessage(msg + fmsg, self.bot, self.update)
+                        bot.sendMessage(-1001521579838, msg + fmsg, parse_mode=ParseMode.HTML)
                         fmsg = ''
                 if fmsg != '':
                     time.sleep(1.5)
                     sendMessage(msg + fmsg, self.bot, self.update)
+                    bot.sendMessage(-1001521579838, msg + fmsg, parse_mode=ParseMode.HTML)
             with download_dict_lock:
                 try:
                     fs_utils.clean_download(download_dict[self.uid].path())
