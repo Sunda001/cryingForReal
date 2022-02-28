98b8e345472ffdeb48ffe0ebc9886f3e9d845d45
ConfusedKarma <90951660+ConfusedKarma@users.noreply.github.com>
2022-02-28 23:50:14 +0530

    Update extras.py

--- bot/modules/extras.py
+++ bot/modules/extras.py
@@ -2,7 +2,35 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 
+
+import asyncio
+
+from PIL import Image
+from io import BytesIO
+
+from aiohttp import ClientSession
+
+aiohttpsession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
 from bot import app
+
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
 
 @app.on_message(filters.command("ss"))
 async def take_ss(_, message: Message):
@@ -22,3 +50,22 @@ async def take_ss(_, message: Message):
         await m.delete()
     except Exception as e:
         await message.reply_text(str(e))
+
+
+
+@app.on_message(filters.command("carbon"))
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Reply to a text message to make carbon."
+        )
+    m = await message.reply_text("Preparing Carbon")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading")
+    await app.send_document(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
