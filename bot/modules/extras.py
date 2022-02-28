6fa14da5a5b146f6363855049974aebaa884e220
ConfusedKarma <90951660+ConfusedKarma@users.noreply.github.com>
2022-03-01 00:12:52 +0530

    Update extras.py

--- bot/modules/extras.py
+++ bot/modules/extras.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 
-
+import os
 import asyncio
 
 from PIL import Image
@@ -29,6 +29,39 @@ async def make_carbon(code):
         image = BytesIO(await resp.read())
     image.name = "carbon.png"
     return image
+
+
+
+
+
+n = "\n"
+w = " "
+
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        text += (
+            indent * w
+            + bold(key)
+            + ((value[0] + n) if isinstance(value, list) else mono(value))
+        )
+    return text
+
+
 
 
 
@@ -69,3 +102,80 @@ async def carbon_func(_, message):
     await app.send_document(message.chat.id, carbon)
     await m.delete()
     carbon.close()
+
+
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
+    if not user.first_name:
+        return ["Deleted account", None]
+    user_id = user.id
+    username = user.username
+    first_name = user.first_name
+    mention = user.mention("Link")
+    dc_id = user.dc_id
+    photo_id = user.photo.big_file_id if user.photo else None
+    is_gbanned = await is_gbanned_user(user_id)
+    is_sudo = user_id in SUDOERS
+    karma = await user_global_karma(user_id)
+    body = {
+        "ID": user_id,
+        "DC": dc_id,
+        "Name": [first_name],
+        "Username": [("@" + username) if username else "Null"],
+        "Mention": [mention],
+    }
+    caption = section("User info", body)
+    return [caption, photo_id]
+
+@app.on_message(filters.command("info"))
+async def info_func(_, message: Message):
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("Processing")
+
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(str(e))
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await app.download_media(photo_id)
+
+    await message.reply_photo(photo, caption=info_caption, quote=False)
+    await m.delete()
+    os.remove(photo)
+
+
+@app.on_message(filters.command("chat_info"))
+async def chat_info_func(_, message: Message):
+    try:
+        if len(message.command) > 2:
+            return await message.reply_text(
+                "**Usage:**/chat_info [USERNAME|ID]"
+            )
+
+        if len(message.command) == 1:
+            chat = message.chat.id
+        elif len(message.command) == 2:
+            chat = message.text.split(None, 1)[1]
+
+        m = await message.reply_text("Processing")
+
+        info_caption, photo_id = await get_chat_info(chat)
+        if not photo_id:
+            return await m.edit(info_caption, disable_web_page_preview=True)
+
+        photo = await app.download_media(photo_id)
+        await message.reply_photo(photo, caption=info_caption, quote=False)
+
+        await m.delete()
+        os.remove(photo)
+    except Exception as e:
+        await m.edit(e)
