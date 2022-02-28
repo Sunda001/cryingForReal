12c85e95b1c8241c4b2f521b550859ab318a5d75
ConfusedKarma <90951660+ConfusedKarma@users.noreply.github.com>
2022-03-01 00:37:39 +0530

    Update extras.py

--- bot/modules/extras.py
+++ bot/modules/extras.py
@@ -199,3 +199,41 @@ async def paste(_, message: Message):
         f"**Posted:** {page['url']}",
         disable_web_page_preview=True,
     )
+
+
+
+@app.on_message(filters.command("id"))
+async def getid(client, message):
+    chat = message.chat
+    your_id = message.from_user.id
+    message_id = message.message_id
+    reply = message.reply_to_message
+
+    text = f"**[Message ID:]({message.link})** `{message_id}`\n"
+    text += f"**[Your ID:](tg://user?id={your_id})** `{your_id}`\n"
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if len(message.command) == 2:
+        try:
+            split = message.text.split(None, 1)[1].strip()
+            user_id = (await client.get_users(split)).id
+            text += f"**[User ID:](tg://user?id={user_id})** `{user_id}`\n"
+        except Exception:
+            return await eor(message, text="This user doesn't exist.")
+
+    text += f"**[Chat ID:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    if not getattr(reply, "empty", True):
+        id_ = reply.from_user.id if reply.from_user else reply.sender_chat.id
+        text += (
+            f"**[Replied Message ID:]({reply.link})** `{reply.message_id}`\n"
+        )
+        text += f"**[Replied User ID:](tg://user?id={id_})** `{id_}`"
+
+    await eor(
+        message,
+        text=text,
+        disable_web_page_preview=True,
+        parse_mode="md",
+    )
