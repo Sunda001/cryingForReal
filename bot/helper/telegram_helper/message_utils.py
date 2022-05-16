2ac6639f6d54375488cf83bec2ee02e5ab239d19
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-05-16 08:36:40 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,77 +1,76 @@
-from time import sleep
 from telegram import InlineKeyboardMarkup
 from telegram.message import Message
-from telegram.error import RetryAfter
-from pyrogram.errors import FloodWait
+from telegram.update import Update
+import psutil, shutil
+import time
+from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, bot, \
+    status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, botStartTime, Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL
+from bot.helper.ext_utils.bot_utils import get_readable_message, get_readable_file_size, get_readable_time, MirrorStatus, setInterval
+from telegram.error import TimedOut, BadRequest, RetryAfter
 
-from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
-                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, bot
-from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
-
-def sendMessage(text: str, bot, message: Message):
+def sendMessage(text: str, bot, update: Update):
     try:
-        return bot.sendMessage(message.chat_id,
-                            reply_to_message_id=message.message_id,
+        return bot.send_message(update.message.chat_id,
+                            reply_to_message_id=update.message.message_id,
                             text=text, allow_sending_without_reply=True, parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
-        LOGGER.warning(str(r))
-        sleep(r.retry_after * 1.5)
-        return sendMessage(text, bot, message)
+        LOGGER.error(str(r))
+        time.sleep(r.retry_after)
+        return sendMessage(text, bot, update)
     except Exception as e:
         LOGGER.error(str(e))
-        return
 
-def sendMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMarkup):
+def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
     try:
-        return bot.sendMessage(message.chat_id,
-                            reply_to_message_id=message.message_id,
-                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True,
+        return bot.send_message(update.message.chat_id,
+                            reply_to_message_id=update.message.message_id,
+                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True, 
                             parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
-        LOGGER.warning(str(r))
-        sleep(r.retry_after * 1.5)
-        return sendMarkup(text, bot, message, reply_markup)
+        LOGGER.error(str(r))
+        time.sleep(r.retry_after)
+        return sendMarkup(text, bot, update, reply_markup)
     except Exception as e:
         LOGGER.error(str(e))
-        return
 
 def editMessage(text: str, message: Message, reply_markup=None):
     try:
-        bot.editMessageText(text=text, message_id=message.message_id,
+        bot.edit_message_text(text=text, message_id=message.message_id,
                               chat_id=message.chat.id,reply_markup=reply_markup,
                               parse_mode='HTMl', disable_web_page_preview=True)
     except RetryAfter as r:
-        LOGGER.warning(str(r))
-        sleep(r.retry_after * 1.5)
+        LOGGER.error(str(r))
+        time.sleep(r.retry_after)
         return editMessage(text, message, reply_markup)
     except Exception as e:
         LOGGER.error(str(e))
-        return
-
 
 def deleteMessage(bot, message: Message):
     try:
-        bot.deleteMessage(chat_id=message.chat.id,
+        bot.delete_message(chat_id=message.chat.id,
                            message_id=message.message_id)
     except Exception as e:
         LOGGER.error(str(e))
 
-def sendLogFile(bot, message: Message):
+
+def sendLogFile(bot, update: Update):
     with open('log.txt', 'rb') as f:
-        bot.sendDocument(document=f, filename=f.name,
-                          reply_to_message_id=message.message_id,
-                          chat_id=message.chat_id)
+        bot.send_document(document=f, filename=f.name,
+                          reply_to_message_id=update.message.message_id,
+                          chat_id=update.message.chat_id)
+
 
 def auto_delete_message(bot, cmd_message: Message, bot_message: Message):
     if AUTO_DELETE_MESSAGE_DURATION != -1:
-        sleep(AUTO_DELETE_MESSAGE_DURATION)
+        time.sleep(AUTO_DELETE_MESSAGE_DURATION)
         try:
             # Skip if None is passed meaning we don't want to delete bot xor cmd message
             deleteMessage(bot, cmd_message)
             deleteMessage(bot, bot_message)
         except AttributeError:
             pass
+
 
 def delete_all_messages():
     with status_reply_dict_lock:
@@ -82,8 +81,32 @@ def delete_all_messages():
             except Exception as e:
                 LOGGER.error(str(e))
 
+
 def update_all_messages():
+    total, used, free = shutil.disk_usage('.')
+    free = get_readable_file_size(free)
+    currentTime = get_readable_time(time.time() - botStartTime)
     msg, buttons = get_readable_message()
+    msg += f"<b>CPU:</b> {psutil.cpu_percent()}% | <b>FREE:</b> {free}"
+    with download_dict_lock:
+        dlspeed_bytes = 0
+        uldl_bytes = 0
+        for download in list(download_dict.values()):
+            speedy = download.speed()
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if 'K' in speedy:
+                    dlspeed_bytes += float(speedy.split('K')[0]) * 1024
+                elif 'M' in speedy:
+                    dlspeed_bytes += float(speedy.split('M')[0]) * 1048576 
+            if download.status() == MirrorStatus.STATUS_UPLOADING:
+                if 'KB/s' in speedy:
+            	    uldl_bytes += float(speedy.split('K')[0]) * 1024
+                elif 'MB/s' in speedy:
+                    uldl_bytes += float(speedy.split('M')[0]) * 1048576
+        dlspeed = get_readable_file_size(dlspeed_bytes)
+        ulspeed = get_readable_file_size(uldl_bytes)
+        msg += f"\n<b>RAM:</b> {psutil.virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}" \
+               f"\n<b>DL:</b> {dlspeed}/s | <b>UL:</b> {ulspeed}/s"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -93,21 +116,45 @@ def update_all_messages():
                     editMessage(msg, status_reply_dict[chat_id], buttons)
                 status_reply_dict[chat_id].text = msg
 
+
 def sendStatusMessage(msg, bot):
     if len(Interval) == 0:
         Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
+    total, used, free = shutil.disk_usage('.')
+    free = get_readable_file_size(free)
+    currentTime = get_readable_time(time.time() - botStartTime)
     progress, buttons = get_readable_message()
+    progress += f"<b>CPU:</b> {psutil.cpu_percent()}% | <b>FREE:</b> {free}"
+    with download_dict_lock:
+        dlspeed_bytes = 0
+        uldl_bytes = 0
+        for download in list(download_dict.values()):
+            speedy = download.speed()
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if 'K' in speedy:
+                    dlspeed_bytes += float(speedy.split('K')[0]) * 1024
+                elif 'M' in speedy:
+                    dlspeed_bytes += float(speedy.split('M')[0]) * 1048576 
+            if download.status() == MirrorStatus.STATUS_UPLOADING:
+                if 'KB/s' in speedy:
+            	    uldl_bytes += float(speedy.split('K')[0]) * 1024
+                elif 'MB/s' in speedy:
+                    uldl_bytes += float(speedy.split('M')[0]) * 1048576
+        dlspeed = get_readable_file_size(dlspeed_bytes)
+        ulspeed = get_readable_file_size(uldl_bytes)
+        progress += f"\n<b>RAM:</b> {psutil.virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}" \
+                    f"\n<b>DL:</b> {dlspeed}/s | <b>UL:</b> {ulspeed}/s"
     with status_reply_dict_lock:
-        if msg.chat.id in list(status_reply_dict.keys()):
+        if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
-                message = status_reply_dict[msg.chat.id]
+                message = status_reply_dict[msg.message.chat.id]
                 deleteMessage(bot, message)
-                del status_reply_dict[msg.chat.id]
+                del status_reply_dict[msg.message.chat.id]
             except Exception as e:
                 LOGGER.error(str(e))
-                del status_reply_dict[msg.chat.id]
+                del status_reply_dict[msg.message.chat.id]
         if buttons == "":
             message = sendMessage(progress, bot, msg)
         else:
             message = sendMarkup(progress, bot, msg, buttons)
-        status_reply_dict[msg.chat.id] = message
+        status_reply_dict[msg.message.chat.id] = message
